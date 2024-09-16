eb9bfd126a0bdc4806cb6d44935e31a01d12447f
igor <igor-123321@yandex.ru>
2024-09-17 01:37:09 +0300

    123

---
+++ https-wolfssl/servercerts.h
@@ -0,0 +1,74 @@
+#define CA_CRT_PEM                                                             \
+    "-----BEGIN CERTIFICATE-----\r\n"                                          \
+    "MIIDSzCCAjOgAwIBAgIUDxRT2hYUsiKJGskvoRByoFS2aTMwDQYJKoZIhvcNAQEL\r\n"     \
+    "BQAwFjEUMBIGA1UEAwwLaW5wb2lzay5jb20wHhcNMjQwOTE0MDkwODE3WhcNMzQw\r\n"     \
+    "OTEyMDkwODE3WjAWMRQwEgYDVQQDDAtpbnBvaXNrLmNvbTCCASIwDQYJKoZIhvcN\r\n"     \
+    "AQEBBQADggEPADCCAQoCggEBAPI9LkY79DvTuAQfte1v1AaBt4tQOHwB7sCxU2T8\r\n"     \
+    "t1xrUsTrTfBDbEvW4Ou2K9n7gFrMoO7a2BAKB+/2ZdssdacNQe4Yb0NDPK29+5Rb\r\n"     \
+    "pkgHTfabz7VCfBKxyJKtFDkQlY8nWQ3UwIf0les3MjDcwR+PmC0IWY2AmMlEyl4D\r\n"     \
+    "2YK+ZxSmWB1EovpGYooIzt3+ET5j7rkLP+VyPRFSUU7QUEeKIkVclo6inq6dz7JS\r\n"     \
+    "KszKrQA5ZmCVyg2L0GfLChNAsh729BeqWfxdQUeobEed2uVm3ShsgnJqH7qegViM\r\n"     \
+    "8zy3sF3YOeycMg3xkAMzqA9Q1l90jPOwqBQs54JDSPLTxj0CAwEAAaOBkDCBjTAd\r\n"     \
+    "BgNVHQ4EFgQUMNlQFFPqDslxMcYw0Gx52FwnBxcwUQYDVR0jBEowSIAUMNlQFFPq\r\n"     \
+    "DslxMcYw0Gx52FwnBxehGqQYMBYxFDASBgNVBAMMC2lucG9pc2suY29tghQPFFPa\r\n"     \
+    "FhSyIokayS+hEHKgVLZpMzAMBgNVHRMEBTADAQH/MAsGA1UdDwQEAwIBBjANBgkq\r\n"     \
+    "hkiG9w0BAQsFAAOCAQEAE6XUaPWo9WTatLPvbXYJ4fyiLdZrAvGzngLsNw9UTnem\r\n"     \
+    "EShoUxm+2uVHq5ntM7onBI4VYaNKBrZGoHOFKpFsc8b8IFp542FRla38pOq/OLA5\r\n"     \
+    "Z0a4h5kYCCr2YYPySB82NNP5GKRNbjsNDQWJhwSyPly1pddx0amQCfaLujGkyAma\r\n"     \
+    "dzQh6SBmirLip3iMcmNWMoGZMHKdX8wzYSzTWab2LRjKA0ImBeU/qtWT/6I4+c9+\r\n"     \
+    "qXYtfzgyVyj4Qud3f0BGkMuviBkkJo1/eXo02OC8o3yZcFNfSV1KD0rC1HeRqcCS\r\n"     \
+    "OJZxpvlNPG4fMJYYahHAJfH9FxA/NjuS4KzitXEheg==\r\n"                         \
+    "-----END CERTIFICATE-----\r\n"
+
+#define SERV_CRT_PEM                                                           \
+    "-----BEGIN CERTIFICATE-----\r\n"                                          \
+    "MIIDcTCCAlmgAwIBAgIQHQ7+UXMgJSKyvgNDCM7+rjANBgkqhkiG9w0BAQsFADAW\r\n"     \
+    "MRQwEgYDVQQDDAtpbnBvaXNrLmNvbTAeFw0yNDA5MTQwOTEwMDNaFw0yNjEyMTgw\r\n"     \
+    "OTEwMDNaMBYxFDASBgNVBAMMC3Rlc3Rfc2VydmVyMIIBIjANBgkqhkiG9w0BAQEF\r\n"     \
+    "AAOCAQ8AMIIBCgKCAQEAvIwAsx/0bFrWZw8XwM3m7dVgtkaGqmU1uanrtLxVNebn\r\n"     \
+    "Oqs2FIwy874Irma0Ba/c70SObOsBavDzgycEI/6hzrg4sHwa/OZu8xRHrvcgpF2B\r\n"     \
+    "7aGKijcgH6HxXF5jXd/BblrCa0iqXPwMghVK/owvf2VlkPbtGu3fAuvYGW+EQHUD\r\n"     \
+    "4SiEAAxIin2QxzYF8vdqyDWjRoDeYBuGIZxKO72p4tsaupYetbTsSg3TP4/Ehz2L\r\n"     \
+    "W7z8iZ2GqijlI4LaGvkCxTjrPcVEWP4vTnumnFzQlH4Re+K/WaSxuGg/grDYDLtt\r\n"     \
+    "LqjyKJ/GvVIk1jn0Hi1IOMbL+u4z4JOd3z76Uiz49QIDAQABo4G6MIG3MAkGA1Ud\r\n"     \
+    "EwQCMAAwHQYDVR0OBBYEFEVLsvIY7NTFcGh24qQ/OdzIki+8MFEGA1UdIwRKMEiA\r\n"     \
+    "FDDZUBRT6g7JcTHGMNBsedhcJwcXoRqkGDAWMRQwEgYDVQQDDAtpbnBvaXNrLmNv\r\n"     \
+    "bYIUDxRT2hYUsiKJGskvoRByoFS2aTMwEwYDVR0lBAwwCgYIKwYBBQUHAwEwCwYD\r\n"     \
+    "VR0PBAQDAgWgMBYGA1UdEQQPMA2CC3Rlc3Rfc2VydmVyMA0GCSqGSIb3DQEBCwUA\r\n"     \
+    "A4IBAQBSATtxR7hwG7V2LjGNOjzZd8WQmq9kLr3OnOj1uWxshEXrQvqDAKYeyiag\r\n"     \
+    "G9EYbXTbNTGitzQ94lq7l0TsHqKDLmlwLktqyu3LPMzEG8gBjvdAzkPsVc/6KbuC\r\n"     \
+    "/iYVqFBrpE1dLzJtk8L3YzKyO2cP2HdZ3LvV2bKUNqJ+y1U5pr5e10nz0bKndf15\r\n"     \
+    "TpfQF8oFT5wn6gcBn2Q4LPmiM8MzI8oLCZqijstw25GMYJ/6/6YLqlBWoR7q1v7c\r\n"     \
+    "tgunzBs993dKgr8ODYMD09WWm58SzFZDe/6HtejwJuboAdQsASZNveeF1Oxw1xv9\r\n"     \
+    "8azFr0zk5atHcDtqu73RwVteODDe\r\n"                                         \
+    "-----END CERTIFICATE-----\r\n"
+
+#define SERV_KEY_PEM                                                           \
+    "-----BEGIN PRIVATE KEY-----\r\n"                                          \
+    "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC8jACzH/RsWtZn\r\n"     \
+    "DxfAzebt1WC2RoaqZTW5qeu0vFU15uc6qzYUjDLzvgiuZrQFr9zvRI5s6wFq8POD\r\n"     \
+    "JwQj/qHOuDiwfBr85m7zFEeu9yCkXYHtoYqKNyAfofFcXmNd38FuWsJrSKpc/AyC\r\n"     \
+    "FUr+jC9/ZWWQ9u0a7d8C69gZb4RAdQPhKIQADEiKfZDHNgXy92rINaNGgN5gG4Yh\r\n"     \
+    "nEo7vani2xq6lh61tOxKDdM/j8SHPYtbvPyJnYaqKOUjgtoa+QLFOOs9xURY/i9O\r\n"     \
+    "e6acXNCUfhF74r9ZpLG4aD+CsNgMu20uqPIon8a9UiTWOfQeLUg4xsv67jPgk53f\r\n"     \
+    "PvpSLPj1AgMBAAECggEACUQVHbh2jeOp8T8l4vELhgksO1D8eb/HE+SEo6l0w5Uh\r\n"     \
+    "LHoxCnH4aA+hBBcihjwGDtEu8a3b8Do+sWWXt8AiChN2H8A9oEufzLzkp/XXqhQ5\r\n"     \
+    "WCkN8qxyXPObtbciyYB/VFHMB7M2wiKV58etm5RemjNsuiLUdLQXd2CJSni3RmaU\r\n"     \
+    "FaVpRO6hoeULwRCHLniuIUqwh0cj1jVEvP696BhsUIsDIXt+8UPTg3TK4iVsnqth\r\n"     \
+    "hJ5BJfz8TTXzbaLDFQKUfrlmEbfQiLi4HMw0o+dqxIwBWaXHVp9ZI8HUCq9ed8+8\r\n"     \
+    "iQrV0CxROk9n/LsrtCZ8tCqEUO79XtpSJsWyBiYiSQKBgQDIEqNvxMt3gz574LhR\r\n"     \
+    "nuItz5whcVhTq0jVKBkBOw4ruD1Y2CbIK8hJ3AOmgWuc5f8iZs8eAeyS8vtMOG7m\r\n"     \
+    "G0reBVhKKvZKt5PqXb4Jcre4WAHXuk2MQE3X4amFZLYY4hZE4AnNmegIvT3L6cRN\r\n"     \
+    "3yeOeJzK8fyjqlNmEjPpDtAGiQKBgQDxQI+JuN3aYrL6bFcssCMZpHWshLn4OnK3\r\n"     \
+    "yhT8/RUe/IJSRMl6w1SjSB8DMnFGAWFeSTiGGqCxukw1LZdnP+kpE739hk8Qer7f\r\n"     \
+    "vDxWUDXSjQ+ZJTnz0TZH+etkus9GwMFi+/zi8TqAkYaQHWPALCEt4qSSGSduAZ3o\r\n"     \
+    "/Az4rM6EDQKBgCV6wF0uu4Lu9iSUq1K29d3pF3F4XziFTMaJlW2Sl4v/HpbnHxS+\r\n"     \
+    "hu23JoPpmLQyrcHPCpuvdpbsp8DWjYfTswm2bJywQaUrkK0F1JTGhb4l1mqIPwC4\r\n"     \
+    "Bx0a6/vOKwKsT8mlQpDedQY2FT5+00RXOnzJAW5POKmcPbVQUaMmIDgpAoGALDBL\r\n"     \
+    "GkrGOA4EAmJ9xS2uADBjbWoSNilegdN97KjeiYJuzGrVDKtigVXgeFFw9gWitAZt\r\n"     \
+    "WALglC0vcD4dkUffiSqYjitfqAETnR95XcXrVdmIleB/sO0NKDnap+SfurHruhm1\r\n"     \
+    "xEuzD4GYSCVZ9AUhPqpYwnnhAwFAzz/7kED2wrECgYBHexLKOocTQPYRyJjbOQ5f\r\n"     \
+    "DXfXB08ZQdXkyZZ8OjfPIPj7Z3DmymAnivbQOc5xDGr+ZuueGwu5fy6C9qwi5cYG\r\n"     \
+    "qrWKzJIndj/rfHVDUJl2NNciNDlmlXzDw19Au/8+J8zi/FKfz6QCkQSQDT9t2O5r\r\n"     \
+    "EpxMbI2+R2448IeuyELwgw==\r\n"                                             \
+    "-----END PRIVATE KEY-----\r\n"    
